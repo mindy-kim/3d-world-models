28799bc5c2662027f88a41239b6a419ef0707496
joobumjin <bumjin.joo@gmail.com>
2025-04-18 11:17:16 -0400

    feat: multiple datagen

--- data_gen.py
+++ data_gen.py
@@ -1,79 +1,41 @@
-import torch
 import numpy as np
-import smplx
 import pickle as pkl
 from moyo.eval.com_evaluation import smplx_to_mesh, visualize_mesh
-from moyo.utils.constants import frame_select_dict_combined as frame_select_dict
 import trimesh
-from human_body_prior.tools.omni_tools import copy2cpu as c2c
-from os import path as osp
 from PIL import Image
 import io
 from fibonacci_sphere import *
 import time
+from tqdm import tqdm
+import os
 
-support_dir = 'data'
+def main():
+    viewpoints = fibonacci_sphere(60)
+    
+    support_dir = 'data'
+    data_folder = "mosh/train/"
 
-# Choose the device to run the body model on.
-comp_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# comp_device
+    for pose_file in os.listdir(f"{support_dir}/{data_folder}")[:1]:
+        pose_name = os.path.splitext(pose_file)[0].split("_", 1)[1]
+        if pose_file.endswith(".pkl"):
+            pp_params = pkl.load(open(f"{support_dir}/{data_folder}/{pose_file}", 'rb'))  
+            num_frames = len(pp_params['fullpose'])
+        else:
+            pp_params = np.load(pose_file)
+            num_frames = len(pp_params['poses'])
+        gender = 'neutral'
+        pp_body_model_output, _, _, faces = smplx_to_mesh(pp_params, f'{support_dir}/models_lockedhead/smplx/SMPLX_NEUTRAL.npz','smplx', gender=gender)
 
-amass_npz_fname = osp.join(support_dir, 'YOGI_2_latest_smplx_neutral/train/220923_yogi_body_hands_03596_Boat_Pose_or_Paripurna_Navasana_-a_stageii.npz') # the path to body data
-bdata = np.load(amass_npz_fname)
+        for frame_ind in tqdm(range(num_frames)[200:201]):
+            pp_mesh = visualize_mesh(pp_body_model_output, faces, frame_id=frame_ind)
+            for view in viewpoints:
+                scene = trimesh.scene.scene.Scene(pp_mesh)
+                scene.set_camera(angles=view, distance=2)
+                png = scene.save_image()
+                with Image.open(io.BytesIO(png)) as img:
+                    # time.sleep(1)
+                    img.save(f"{support_dir}/moyo_png/{pose_name}___t{frame_ind}_v{view}.png") # 3 _'s
+                # scene.reset()
 
-# you can set the gender manually and if it differs from data's then contact or interpenetration issues might happen
-subject_gender = bdata['gender']
-
-print('Data keys available:%s'%list(bdata.keys()))
-
-print('The subject of the mocap sequence is  {}.'.format(subject_gender))
-
-from human_body_prior.body_model.body_model import BodyModel
-
-bm_fname = osp.join(support_dir, 'body_models/smplh/{}/model.npz'.format(subject_gender))
-
-num_betas = 16 # number of body parameters
-
-bm = BodyModel(f'{support_dir}/models_lockedhead/smplx/SMPLX_NEUTRAL.npz', 'smplx', num_betas=num_betas).to(comp_device)
-faces = c2c(bm.f)
-
-time_length = len(bdata['trans'])
-
-body_parms = {
-    'root_orient': torch.Tensor(bdata['poses'][:, :3]).to(comp_device), # controls the global root orientation
-    'pose_body': torch.Tensor(bdata['poses'][:, 3:66]).to(comp_device), # controls the body
-    'pose_hand': torch.Tensor(bdata['poses'][:, 66:]).to(comp_device), # controls the finger articulation
-    'trans': torch.Tensor(bdata['trans']).to(comp_device), # controls the global body position
-    'betas': torch.Tensor(np.repeat(bdata['betas'][:num_betas][np.newaxis], repeats=time_length, axis=0)).to(comp_device), # controls the body shape. Body shape is static
-}
-
-print('Body parameter vector shapes: \n{}'.format(' \n'.join(['{}: {}'.format(k,v.shape) for k,v in body_parms.items()])))
-print('time_length = {}'.format(time_length))
-
-mosh = f"{support_dir}/mosh/train/220923_yogi_body_hands_03596_Boat_Pose_or_Paripurna_Navasana_-a_stageii.pkl"
-
-# all_pose_names = [pp_name.replace('_stageii', '') for pp_name in pp_names]
-selected_frame = frame_select_dict['_'.join(mosh.split('_')[5:-1])]
-
-pp_pkl = mosh
-pp_params = pkl.load(open(pp_pkl, 'rb'))
-# read height, weight, gender meta
-height = None
-weight = None
-gender = 'neutral'
-subj_id = 'unknown'
-pp_body_model_output, smplx_params, transls, faces = smplx_to_mesh(pp_params, f'{support_dir}/models_lockedhead/smplx/SMPLX_NEUTRAL.npz','smplx', gender=gender)
-
-pp_mesh = visualize_mesh(pp_body_model_output, faces, frame_id=550)
-
-viewpoints = fibonacci_sphere(60)
-
-for view in viewpoints:
-    scene = trimesh.scene.scene.Scene(pp_mesh)
-    scene.set_camera(angles=view, distance=2)
-    png = scene.save_image()
-    with Image.open(io.BytesIO(png)) as img:
-        time.sleep(1)
-    # scene.reset()
-# image.save("./sample.png")
-# print(f"PNG file saved as {"./sample.png"}")
+if __name__ == "__main__":
+    main()
