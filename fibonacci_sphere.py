6229e91423dcab487b45063deadec39b473207c0
gaga1313 <gauravgaonkar13.13@gmail.com>
2025-04-17 22:29:51 -0400

    fibonacci sphere

---
+++ fibonacci_sphere.py
@@ -0,0 +1,13 @@
+# Sample uniformly on a sphere
+def fibonacci_sphere(samples=100):
+    points = []
+    phi = np.pi * (3. - np.sqrt(5.))  # golden angle in radians
+    for i in range(samples):
+        y = 1 - (i / float(samples - 1)) * 2  # y goes from 1 to -1
+        radius = np.sqrt(1 - y * y)
+        theta = phi * i
+        x = np.cos(theta) * radius
+        z = np.sin(theta) * radius
+        points.append([x, y, z])
+    return np.array(points)
+
